ba0f5b71e6623c18657854a00b71e80592b75431
Frank Jung <frankhjung@gmail.com>
2015-05-18 10:06:10 +1000

    plot2 use with

--- plot2a.R
+++ plot2a.R
@@ -12,12 +12,12 @@ nei <- readRDS("data/summarySCC_PM25.rds")
 totals <- aggregate(Emissions ~ year, data = subset(nei, fips == "24510"), sum)
 
 png(filename = "plot2a.png", width = 480, height = 480, units = "px")
-attach(totals)
-plot(year, Emissions, type = "b", xaxt = "n", xlab = "Year", ylab = "Emissions (Tons)")
-axis(1, at = year)
-abline(lm(Emissions ~ year, totals), lty = 3)
-title(main = expression("Baltimore City, Maryland: " * PM[2.5] * " Emissions from all Sources"))
-detach(totals)
+with(totals,
+     plot(year, Emissions, type = "b", xaxt = "n", xlab = "Year", ylab = "Emissions (Tons)") +
+     axis(1, at = year) +
+     abline(lm(Emissions ~ year, totals), lty = 3) +
+     title(main = expression("Baltimore City, Maryland: " * PM[2.5] * " Emissions from all Sources"))
+)
 dev.off()
 
 rm(totals)
