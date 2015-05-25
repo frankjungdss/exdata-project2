879be7adff31c79f4bf5149c0e09c697ec18b704
Frank Jung <frankhjung@gmail.com>
2015-05-25 15:56:54 +1000

    plot5 use ei sector as a emissions category

--- plot5a.R
+++ plot5a.R
@@ -15,24 +15,25 @@ nei <- readRDS("data/summarySCC_PM25.rds")
 scc <- readRDS("data/Source_Classification_Code.rds")
 
 # get SCC (source code classification) digits for mobile sources
-vehiclescc <- as.character(scc[grepl("(?=.*Mobile - )(?=.*-Road)", scc$EI.Sector, perl = T), "SCC"])
+vehiclescc <- scc[grepl("(?=.*Mobile - )(?=.*-Road)", scc$EI.Sector, perl = T), c("SCC", "EI.Sector")]
 
-# aggregate emissions by year
+# aggregate emissions by year by sector
 totals <- nei %>%
     filter(fips == "24510") %>%
-    filter(SCC %in% vehiclescc) %>%
-    select(year, type, Emissions) %>%
-    arrange(year, type) %>%
-    group_by(year, type) %>%
+    inner_join(y = vehiclescc, by = "SCC") %>%
+    mutate(sector = sub("Mobile - ", "", EI.Sector)) %>%
+    select(year, sector, Emissions) %>%
+    arrange(year, sector) %>%
+    group_by(year, sector) %>%
     summarise(total = sum(Emissions))
 
-# plot stacked bar graph for each year of emission source types totals
-png(filename = "plot5a.png", width = 640, height = 480, units = "px")
+# plot stacked bar graph for each year by emission sector
+png(filename = "plot5a.png", width = 720, height = 640, units = "px")
 totals %>%
-    ggplot(aes(year, total, fill = tolower(type))) +
+    ggplot(aes(year, total, fill = sector)) +
     geom_bar(stat = "identity", position = "stack") +
     theme_light(base_family = "Avenir", base_size = 11) +
-    scale_fill_brewer("Emission Source Type", palette = "Set1") +
+    scale_fill_brewer("Emission Sector", palette = "Set1") +
     scale_x_continuous("Year", breaks = totals$year) +
     scale_y_continuous("Emissions (Tons)", breaks = pretty_breaks(n = 10)) +
     ggtitle(expression("Baltimore City, Maryland:" * PM[2.5] * " Emissions from Motor Vehicle Sources"))
